db165472b30c3a5224a6c763926bf4a8514200a2
Paul Kernfeld <pk@knewton.com>
2014-05-10 01:17:34 -0400

    Add noweats, switch to node-style list

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 var urls = [
-  "http://getaddr.bitnodes.io/",
-  "http://aworldoftweets.frogdesign.com/",
-  "http://www.tweetping.net/",
-  "http://hint.fm/wind/",
-  "http://map.honeycloud.net/",
-  "http://earth.nullschool.net/",
-  "http://world.deviantart.com/",
-  "http://bostonography.com/bus/",
-  "http://www.akamai.com/html/technology/dataviz1.html",
-  "http://bitcoinity.org/markets",
-  "http://www.listentobitcoin.com/",
-  "http://www.google.com/trends/hottrends/visualize?nrow=5&ncol=5",
-  "https://map.zkillboard.com/"
+  "http://getaddr.bitnodes.io/"
+  , "http://aworldoftweets.frogdesign.com/"
+  , "http://www.tweetping.net/"
+  , "http://hint.fm/wind/"
+  , "http://map.honeycloud.net/"
+  , "http://earth.nullschool.net/"
+  , "http://world.deviantart.com/"
+  , "http://bostonography.com/bus/"
+  , "http://www.akamai.com/html/technology/dataviz1.html"
+  , "http://bitcoinity.org/markets"
+  , "http://www.listentobitcoin.com/"
+  , "http://www.google.com/trends/hottrends/visualize?nrow=5&ncol=5"
+  , "https://map.zkillboard.com/"
+  , "http://brandonreiss.com/noweats/"
 ];
 
 setTimeout(function() {
