f0f696545cedb6d41e1db45b2540c53b7625d676
Paul Kernfeld <pk@knewton.com>
2015-04-21 13:09:15 -0400

    Listen to Bitcoin is no longer up

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var urls = [
   , "http://bostonography.com/bus/"
   , "http://www.akamai.com/html/technology/dataviz1.html"
   , "http://bitcoinity.org/markets"
-  , "http://www.listentobitcoin.com/"
   , "http://www.google.com/trends/hottrends/visualize?nrow=5&ncol=5"
   , "https://map.zkillboard.com/"
   , "http://brandonreiss.com/noweats/"
